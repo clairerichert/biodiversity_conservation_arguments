aea2b1b1de560ee769544bdc3fc899b2f46fd0c5
Claire Richert <richert.claire@gmail.com>
2024-10-20 20:32:28 +0200

    figure with arguments frequency

--- script/arguments_analysis.R
+++ script/arguments_analysis.R
@@ -44,8 +44,7 @@ for (prem in all_premises) {
   }
 }
 
-obs_by_premise <- obs_by_premise[order(obs_by_premise[ , 'biodiversity'], decreasing = TRUE), ]
-
+obs_by_premise <- obs_by_premise[order(obs_by_premise[, 'biodiversity'], decreasing = TRUE),]
 write.csv(obs_by_premise, 'results/number_obs_by_premise.csv')
 
 # analysis of how the premises are combined
@@ -64,12 +63,35 @@ data_q18 <- build_arguments(data_q18)
 comb_q3 <- argument_analysis('q3', data_q3$arguments)
 comb_q18 <- argument_analysis('q18', data_q18$arguments)
 
+# plot the number of respondents by argument
+all_args <- merge(comb_q3, comb_q18, by = "argument", suffixes = c(".biodiv", ".insects"), all.x = TRUE, all.y = TRUE)
+all_args[is.na(all_args)] <- 0
+all_args <- all_args[all_args$Prop.biodiv >= 0.05 | all_args$Prop.insects >= 0.05,]
+insects <- data.frame(frequency = all_args$Prop.insects, argument = all_args$argument, protection_type = "insects")
+biodiv <- data.frame(frequency = all_args$Prop.biodiv, argument = all_args$argument, protection_type = "biodiversity")
+obs_by_arg <- rbind(insects, biodiv)
+values_for_ordering <- obs_by_arg[obs_by_arg$protection_type == "biodiversity", "frequency"]
+obs_by_arg$col_to_reorder <- c(values_for_ordering, values_for_ordering)
+
+plot_premise_frequency <- ggplot(data = obs_by_arg,
+                                 aes(x = frequency,
+                                     y = reorder(argument, col_to_reorder), fill = protection_type)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  ylab("argument") +
+  xlab("frequency") +
+  scale_fill_grey(name = waiver(), start = 0.2, end = 0.8, aesthetics = "fill") +
+  theme(legend.text = element_text(size = 14), legend.title = element_text(size = 16),
+        axis.text.y = element_text(size = 14), axis.text.x = element_text(size = 14), text = element_text(size = 14))
+
+ggsave(file.path("results", "argument_frequency.png"), width = 12, height = 9)
+
 # Categorize the arguments
 data_q3 <- categorize_arguments(data_q3)
 data_q18 <- categorize_arguments(data_q18)
 
 # Plot the frequency of each category for biodiversity and insects protection
 cat <- c('INSTRUMENTAL_HUMANS', 'INSTRUMENTAL_GENERAL', 'INSTRUMENTAL_UNSPECIFIED', 'NON_INSTRUMENTAL')
+
 build_df_protection_type_frequency_cat <- function(df, protection_type) {
   return(data.frame(
     frequency = apply(df[, cat], 2, function(x) { sum(x == 1) / nrow(df) }),
@@ -81,18 +103,18 @@ df_freq <- do.call(rbind, mapply(function(d, pt) { build_df_protection_type_freq
                                  list(data_q3, data_q18), c("biodiversity", "insects"), SIMPLIFY = FALSE))
 
 plot_cat_frequency <- ggplot(data = df_freq, aes(x = frequency, y = category, fill = protection_type)) +
-  geom_bar(stat="identity", position=position_dodge()) +
+  geom_bar(stat = "identity", position = position_dodge()) +
   scale_fill_grey(name = waiver(), start = 0.2, end = 0.8, aesthetics = "fill") +
   theme(legend.text = element_text(size = 14), legend.title = element_text(size = 16),
         axis.text.y = element_text(size = 14), axis.text.x = element_text(size = 14), text = element_text(size = 14))
 
 # sankey for categories between Q3 and Q18
-common <- merge(data_q3[ , c(cat, 'id_final')], data_q18[ , c(cat, 'id_final')], by = 'id_final', suffixes = c('_biodiversity', '_insects'))
+common <- merge(data_q3[, c(cat, 'id_final')], data_q18[, c(cat, 'id_final')], by = 'id_final', suffixes = c('_biodiversity', '_insects'))
 
 define_argument_selected <- function(df, suffix) {
-  sdf <- df[ , grep(suffix, colnames(df))]
+  sdf <- df[, grep(suffix, colnames(df))]
   colnames(sdf) <- gsub(suffix, '', colnames(sdf))
-  return(apply(sdf, 1, function(x) {colnames(sdf)[x == 1]}))
+  return(apply(sdf, 1, function(x) { colnames(sdf)[x == 1] }))
 }
 
 sankey_df <- data.frame('biodiversity' = define_argument_selected(common, '_biodiversity'),
@@ -101,11 +123,14 @@ sankey_df <- sankey_df %>%
   make_long(biodiversity, insects)
 
 plot_sankey <- ggplot(sankey_df, aes(x = x, next_x = next_x, node = node, next_node = next_node,
-               fill = factor(node), label = node)) +
-  geom_sankey(show.legend = FALSE) + theme_sankey(base_size = 14) + geom_sankey_label(color = "black", fill = "white") + labs(x = NULL) +
+                                     fill = factor(node), label = node)) +
+  geom_sankey(show.legend = FALSE) +
+  theme_sankey(base_size = 14) +
+  geom_sankey_label(color = "black", fill = "white") +
+  labs(x = NULL) +
   theme(legend.position = "none", axis.text.x = element_text(size = 16)) +
   scale_fill_grey(name = waiver(), start = 0.2, end = 0.8, aesthetics = "fill")
 
 ggarrange(plotlist = list(plot_cat_frequency, plot_sankey), ncol = 1, nrow = 2, labels = list("A.", "B."), label.x = -0.02) +
-  theme(plot.margin = margin(0,0,0,0.5, "cm"))
+  theme(plot.margin = margin(0, 0, 0, 0.5, "cm"))
 ggsave(file.path("results", "sankey_and_barplot_categories_of_arguments.png"), width = 12, height = 9)
